9184143c2eaf144f79aacf5ba9ba2bed3bc77522
giovanism <giovanism@outlook.co.id>
2018-10-28 16:38:02 +0700

    Add insults from sudo source code

--- tsudo/lines.py
+++ tsudo/lines.py
@@ -28,3 +28,113 @@ tsun_lines = (
     "HEY! It's a privilege to even be able to talk to me! You should be honored!",
     "Geez, stop pushing yourself! You're going to get yourself hurt one day, you idiot!"
 )
+
+ins_2001 = (
+
+    # HAL insults (paraphrased) from 2001.
+    # taken from sudo/plugins/sudoers/ins_2001.h
+
+    "Just what do you think you're doing Dave?",
+    "It can only be attributed to human error.",
+    "That's something I cannot allow to happen.",
+    "My mind is going. I can feel it.",
+    "Sorry about this, I know it's a bit silly.",
+    "Take a stress pill and think things over.",
+    "This mission is too important for me to allow you to jeopardize it.",
+    "I feel much better now.",
+)
+
+ins_goons = (
+
+     # Insults from the "Goon Show."
+     # taken from sudo/plugins/sudoers/ins_goons.h
+
+    "You silly, twisted boy you.",
+    "He has fallen in the water!",
+    "We'll all be murdered in our beds!",
+    "You can't come in. Our tiger has got flu",
+    "I don't wish to know that.",
+    "What, what, what, what, what, what, what, what, what, what?",
+    "You can't get the wood, you know.",
+    "You'll starve!",
+    "... and it used to be so popular...",
+    "Pauses for audience applause, not a sausage",
+    "Hold it up to the light --- not a brain in sight!",
+    "Have a gorilla...",
+    "There must be cure for it!",
+    "There's a lot of it about, you know.",
+    "You do that again and see what happens...",
+    "Ying Tong Iddle I Po",
+    "Harm can come to a young lad like that!",
+    "And with that remarks folks, the case of the Crown vs yourself was proven.",
+    "Speak English you fool --- there are no subtitles in this scene.",
+    "You gotta go owwwww!",
+    "I have been called worse.",
+    "It's only your word against mine.",
+    "I think ... err ... I think ... I think I'll go home",
+)
+
+ins_classic = (
+
+    # Insults from the original sudo(8).
+    # taken from sudo/plugins/sudoers/ins_classic.h
+
+    "Wrong!  You cheating scum!",
+    "And you call yourself a Rocket Scientist!",
+    "No soap, honkie-lips.",
+    "Where did you learn to type?",
+    "Are you on drugs?",
+    "My pet ferret can type better than you!",
+    "You type like i drive.",
+    "Do you think like you type?",
+    "Your mind just hasn't been the same since the electro-shock, has it?",
+)
+
+ins_csops = (
+
+     # CSOps insults (may be site dependent).
+     # taken from sudo/plugins/sudoers/ins_csops.h
+
+    "Maybe if you used more than just two fingers...",
+    "BOB says:  You seem to have forgotten your passwd, enter another!",
+    "stty: unknown mode: doofus",
+    "I can't hear you -- I'm using the scrambler.",
+    "The more you drive -- the dumber you get.",
+    "Listen, broccoli brains, I don't have time to listen to this trash.",
+    "Listen, burrito brains, I don't have time to listen to this trash.",
+    "I've seen penguins that can type better than that.",
+    "Have you considered trying to match wits with a rutabaga?",
+    "You speak an infinite deal of nothing",
+)
+
+
+
+ins_python = (
+
+     # Insults from the "Goon Show."
+     # taken from sudo/plugins/sudoers/ins_python.h
+
+    "You silly, twisted boy you.",
+    "He has fallen in the water!",
+    "We'll all be murdered in our beds!",
+    "You can't come in. Our tiger has got flu",
+    "I don't wish to know that.",
+    "What, what, what, what, what, what, what, what, what, what?",
+    "You can't get the wood, you know.",
+    "You'll starve!",
+    "... and it used to be so popular...",
+    "Pauses for audience applause, not a sausage",
+    "Hold it up to the light --- not a brain in sight!",
+    "Have a gorilla...",
+    "There must be cure for it!",
+    "There's a lot of it about, you know.",
+    "You do that again and see what happens...",
+    "Ying Tong Iddle I Po",
+    "Harm can come to a young lad like that!",
+    "And with that remarks folks, the case of the Crown vs yourself was proven.",
+    "Speak English you fool --- there are no subtitles in this scene.",
+    "You gotta go owwwww!",
+    "I have been called worse.",
+    "It's only your word against mine.",
+    "I think ... err ... I think ... I think I'll go home",
+)
